e4c19c8ff93d1bd3351b28a2dc673b8ca7c5ed0b
b4ldorDev <A01773374@tec.mx>
2025-01-21 17:34:34 -0600

    modificación del next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  
+  basePath: '/Portfolio_ingenier0espacial',
+  assetPrefix: '/Portfolio_ingenier0espacial/',
+  reactStrictMode: true, 
 };
 
 export default nextConfig;
